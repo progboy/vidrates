85b81c94657349574543c86e515d7dd59789ab85
progboy <20139582+progboy@users.noreply.github.com>
2025-03-13 13:52:53 +0530

    implemented rating selector, linked the css file

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,67 @@
 let star1 = document.getElementById("star1");
+let star2 = document.getElementById("star2");
+let star3 = document.getElementById("star3");
+let star4 = document.getElementById("star4");
+let star5 = document.getElementById("star5");
+
+let rating = 0;
+
+let submit = document.getElementById("submit");
+
+function setStar(star){
+    star.src = "./images/star_shaded.png";
+}
+function unsetStar(star){
+    star.src = "./images/star_unshaded.png";
+}
 star1.onclick = function(){
-    console.log(star1.src);
-    if(star1.src.includes("unshaded")){
-        star1.src="./images/star_shaded.png";
-    }else{
-        star1.src="./images/star_unshaded.png";
-    }
+    rating = 1;
+    console.log(rating);
+    setStar(star1);
+    unsetStar(star2);
+    unsetStar(star3);
+    unsetStar(star4);
+    unsetStar(star5);
 };
+star2.onclick = function(){
+    rating = 2;
+    console.log(rating);
+    setStar(star1);
+    setStar(star2);
+    unsetStar(star3);
+    unsetStar(star4);
+    unsetStar(star5);
+};
+star3.onclick = function(){
+    rating = 3;
+    console.log(rating);
+    setStar(star1);
+    setStar(star2);
+    setStar(star3);
+    unsetStar(star4);
+    unsetStar(star5);
+};
+star4.onclick = function(){
+    rating = 4;
+    console.log(rating);
+    setStar(star1);
+    setStar(star2);
+    setStar(star3);
+    setStar(star4);
+    unsetStar(star5);
+};
+star5.onclick = function(){
+    rating = 5;
+    console.log(rating);
+    setStar(star1);
+    setStar(star2);
+    setStar(star3);
+    setStar(star4);
+    setStar(star5);
+};
+
+submit.onclick = () => {
+    console.log(rating);
+    //from here data is sent to backend
+    //to do - implement rating should be associated with video links
+}
